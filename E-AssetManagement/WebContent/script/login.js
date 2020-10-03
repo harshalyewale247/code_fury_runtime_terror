65a026f78aec6688e6016b98f4e752da6bd03d06
harshalyewale247 <harshal.yewale24@gmail.com>
2020-10-03 21:45:20 +0530

    login UI added

---
+++ E-AssetManagement/WebContent/script/login.js
@@ -0,0 +1,16 @@
+window.addEventListener('load',function()
+{
+document.querySelector("#userLogin").addEventListener('submit',
+        function()
+    {
+        //console.log("login successfull !!");
+        alert("login successfull !!");
+
+    })
+
+    
+})
+
+function login()
+{ 
+}
